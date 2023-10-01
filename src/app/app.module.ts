83d4ec9d6c6a73b08cc36526ff2b59976bca4b52
mysticBel <dnxbel@gmail.com>
2023-10-01 08:41:39 -0500

    iniciando proyecto con app module modalidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddModalidadComponent
   ],
   imports: [
     BrowserModule
