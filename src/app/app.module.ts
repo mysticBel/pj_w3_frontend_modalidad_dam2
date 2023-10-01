1df2ac1db3208ed3b7f63255ff114a20992f909a
mysticBel <dnxbel@gmail.com>
2023-10-01 09:31:56 -0500

    fin part2 -vinculando app modalidad a vacbar, ya apaerece componente form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AddModalidadComponent } from './components/add-modalidad/add-modalidad.
     AddModalidadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
