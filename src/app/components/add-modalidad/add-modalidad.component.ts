adad151c12c95b1d9e76212acc50783ab658de5f
mysticBel <dnxbel@gmail.com>
2023-10-01 10:06:17 -0500

    en componente add-modalidad, agrega service y  metodo suscribe

--- src/app/components/add-modalidad/add-modalidad.component.ts
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Deporte } from 'src/app/models/deporte.model';
+import { DeporteService } from 'src/app/services/deporte.service';
 
 @Component({
   selector: 'app-add-modalidad',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-modalidad.component.css']
 })
 export class AddModalidadComponent {
+ // Creamos un arreglo vacio para inicializarlo
+  lstDeporte : Deporte[] = [];
 
+  //agregamos constructor para el Service
+   constructor(private deporteService: DeporteService){
+
+      this.deporteService.listaDeporte().subscribe(
+          data => this.lstDeporte = data
+      );
+   }
 }
