0547c651990f59ae8e7dc278df2c8717277fb70c
mysticBel <dnxbel@gmail.com>
2023-10-01 10:44:49 -0500

    agregamos en modalidadServide el metodoregistraModalidad

--- src/app/services/modalidad.service.ts
+++ src/app/services/modalidad.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AppSettings } from '../app.settings';
+import { HttpClient } from '@angular/common/http';
+import { Modalidad } from '../models/modalidad.model';
+//
+import { Observable } from 'rxjs';
+//
+const url = AppSettings.API_ENDPOINT + '/modalidad'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalidadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //metodo post
+  registraModalidad(obj:Modalidad): Observable<any>{
+    return this.http.post(url, obj);
+}
 }
